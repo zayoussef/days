2e5e87514cefc7ff3d5b356d75e53a6ec9f6dc03
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-09-07 16:14:41 -0400

    Update ft_putnbr_base.c

--- DAYS/C04/ex04/ft_putnbr_base.c
+++ DAYS/C04/ex04/ft_putnbr_base.c
@@ -1,60 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yozainan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/22 23:30:38 by yozainan          #+#    #+#             */
+/*   Updated: 2023/08/26 16:12:56 by yozainan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-int is_valid_base(char *base)
+void	ft_putchar(char c)
 {
-    int i = 0;
-    while (base[i] != '\0')
-    {
-        if (base[i] == '+' || base[i] == '-' || base[i] < 32 || base[i] > 126)
-            return 0;
-
-        int j = i + 1;
-        while (base[j] != '\0')
-        {
-            if (base[i] == base[j])
-                return 0;
-            j++;
-        }
-
-        i++;
-    }
-    return (i > 1);
+	write (1, &c, 1);
 }
 
-void ft_putnbr_base(int nbr, char *base)
+int	ft_strlen(char *str)
 {
-    if (!is_valid_base(base))
-        return;
+	int	i;
 
-    int base_length = 0;
-    while (base[base_length] != '\0')
-        base_length++;
-
-    if (nbr < 0)
-    {
-        write(1, "-", 1);
-        nbr = -nbr;
-    }
-
-    if (nbr >= base_length)
-    {
-        ft_putnbr_base(nbr / base_length, base);
-        ft_putnbr_base(nbr % base_length, base);
-    }
-    else
-    {
-        char digit = base[nbr];
-        write(1, &digit, 1);
-    }
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
-int main(void)
+int	the_base(char *str)
 {
-    int number = 12345;
-    char base[] = "0123456789ABCDEF";
+	int	i;
+	int	j;
+	int	len;
 
-    ft_putnbr_base(number, base);
-
-    return 0;
+	i = 0;
+	len = ft_strlen(str);
+	if (str[0] == '\0' || len == 1)
+		return (0);
+	while (str[i] != '\0')
+	{
+		if (str[i] <= 32 || str[i] == 127 || str[i] == '-' || str[i] == '+')
+			return (0);
+		j = i + 1;
+		while (j < len)
+		{
+			if (str[i] == str[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
 
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int		len;
+	long	nb;
+	int		cheek;
+
+	cheek = the_base(base);
+	len = ft_strlen(base);
+	nb = (long) nbr;
+	if (cheek == 1)
+	{
+		if (nb < 0)
+		{
+			ft_putchar ('-');
+			nb *= -1;
+		}
+		if (nb < len)
+			ft_putchar (base[nb]);
+		if (nb >= len)
+		{
+			ft_putnbr_base(nb / len, base);
+			ft_putnbr_base(nb % len, base);
+		}
+	}
+}
