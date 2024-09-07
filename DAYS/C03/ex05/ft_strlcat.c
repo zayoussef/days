d57b86a00d25212bf343aaee72f1969939e0eb8b
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-09-07 16:12:58 -0400

    Update ft_strlcat.c

--- DAYS/C03/ex05/ft_strlcat.c
+++ DAYS/C03/ex05/ft_strlcat.c
@@ -1,54 +1,56 @@
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	len_d;
+	unsigned int	len_s;
+
+	i = 0;
+	len_d = ft_strlen(dest);
+	len_s = ft_strlen(src);
+	if (size == 0 || size <= len_d)
+		return (len_s + size);
+	j = len_d;
+	while (src[i] != '\0' && i < size - len_d - 1)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (len_d + len_s);
+}
+/*#include <string.h>
 #include <stdio.h>
-
-unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
+int main()
 {
-    unsigned int dest_length = 0;
-    unsigned int src_length = 0;
-    unsigned int total_length = 0;
-
-    // Find the length of the destination string
-    while (dest[dest_length] != '\0' && dest_length < size)
-    {
-        dest_length++;
-    }
-
-    // Find the length of the source string
-    while (src[src_length] != '\0')
-    {
-        src_length++;
-    }
-
-    total_length = dest_length + src_length;
-
-    if (dest_length < size)
-    {
-        unsigned int i = 0;
-        while (src[i] != '\0' && dest_length < size - 1)
-        {
-            dest[dest_length] = src[i];
-            dest_length++;
-            i++;
-        }
-        dest[dest_length] = '\0';
-    }
-
-    return total_length;
-}
-
-int main(void)
-{
-    char dest[20] = "Hello";
-    char src[] = ", World!";
-    unsigned int size = sizeof(dest) / sizeof(dest[0]);
-
-    printf("Destination string before concatenation: %s\n", dest);
-    printf("Source string: %s\n", src);
-
-    unsigned int result = ft_strlcat(dest, src, size);
-
-    printf("Concatenated string: %s\n", dest);
-    printf("Total length: %u\n", result);
-
-    return 0;
-}
-
+	char	src[] = "youssef";
+	char	dest[10] = "cv";
+	printf("src: %s\n", src);
+	printf("dest: %s\n", dest);
+	printf("%d\n", ft_strlcat(dest, src, 5));
+	printf("dest:%s\n", dest);
+	printf("src:%s\n", src);
+	printf("===============================================\n");
+	char	ss[] = "youssef";
+	char	dd[10] = "cv";
+	printf("ss:  %s\n", ss);
+	printf("dd:  %s\n", dd);
+	printf("%lu\n", strlcat(dd, ss, 5));
+	printf("dd1:%s\n", dd);
+	printf("ss1:%s\n", ss);	
+	return (0);
+}*/
