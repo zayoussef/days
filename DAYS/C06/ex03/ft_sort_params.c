052b72601029c8a6dd8502855eaa5408c7d14ac0
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-09-08 23:26:03 +0100

    Update ft_sort_params.c

--- DAYS/C06/ex03/ft_sort_params.c
+++ DAYS/C06/ex03/ft_sort_params.c
@@ -1,61 +1,63 @@
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_print_sort(int ac, char **av)
 {
-    write(1, &c, 1);
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j] != '\0')
+		{
+			write (1, &av[i][j], 1);
+			j++;
+		}
+		write (1, "\n", 1);
+		i++;
+	}
 }
 
-void ft_putstr(char *str)
+int	ft_strcmp(char *s1, char *s2)
 {
-    int i = 0;
-    while (str[i] != '\0')
-        ft_putchar(str[i++]);
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && (s1[i] || s2[i]))
+	{
+		i++;
+	}
+	if (s1[i] == s2[i])
+		return (0);
+	else
+	{
+		return (s1[i] - s2[i]);
+	}
 }
 
-int ft_strcmp(char *s1, char *s2)
+int	main(int ac, char **av)
 {
-    int i = 0;
-    while (s1[i] != '\0' || s2[i] != '\0')
-    {
-        if (s1[i] == s2[i])
-            i++;
-        if (s1[i] < s2[i] || s1[i] > s2[i])
-            return (s1[i] - s2[i]);
-    }
-    return (0);
-}
+	int		i;
+	int		j;
+	char	*tmp;
 
-void ft_swap(char **a, char **b)
-{
-    char *tmp = *a;
-    *a = *b;
-    *b = tmp;
+	i = 1;
+	while (i < ac - 1)
+	{
+		j = 1;
+		while (j < ac - i)
+		{
+			if (ft_strcmp(av[j], av[j + 1]) > 0)
+			{
+				tmp = av[j];
+				av[j] = av[j + 1];
+				av[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+	ft_print_sort(ac, av);
+	return (0);
 }
-
-int main(int argc, char **argv)
-{
-    int j, x;
-    x = 1;
-    while (x < argc - 1)
-    {
-        j = 1;
-        while (j < argc - 1)
-        {
-            if (ft_strcmp(argv[j], argv[j + 1]) > 0)
-            {
-                ft_swap(&argv[j], &argv[j + 1]);
-            }
-            j++;
-        }
-        x++;
-    }
-    j = 1;
-    while (j < argc)
-    {
-        ft_putstr(argv[j]);
-        ft_putchar('\n');
-        j++;
-    }
-    return 0;
-}
-
