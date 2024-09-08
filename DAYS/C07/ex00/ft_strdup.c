bfd0b79c4e7015aa1e2c88667050a8f33630658d
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-09-08 23:31:08 +0100

    Update ft_strdup.c

--- DAYS/C07/ex00/ft_strdup.c
+++ DAYS/C07/ex00/ft_strdup.c
@@ -1,42 +1,29 @@
 #include <stdlib.h>
-#include <stdio.h>
-char *ft_strdup(char *src) {
-    if (src == NULL) // Handle NULL case
-        return NULL;
 
-    size_t len = 0;
-    while (src[len] != '\0') // Find the length of the source string
-        len++;
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*ptr;
 
-    // Allocate memory for the new string (including null terminator)
-    char *dest = (char *)malloc((len + 1) * sizeof(char));
-
-    if (dest == NULL) // Handle memory allocation failure
-        return NULL;
-
-    size_t i = 0;
-    while (src[i] != '\0') {
-        dest[i] = src[i]; // Copy characters from source to destination
-        i++;
-    }
-
-    dest[i] = '\0'; // Add null terminator
-
-    return dest;
+	i = 0;
+	while (src[i])
+		i++;
+	ptr = (char *) malloc((i + 1) * sizeof(char));
+	if (ptr != NULL)
+	{
+		i = 0;
+		while (src[i])
+		{
+			ptr[i] = src[i];
+			i++;
+		}
+		ptr[i] = '\0';
+		return (ptr);
+	}
+	return (0);
 }
-
-int main() {
-    char *src = "Hello, world!";
-    char *copy = ft_strdup(src);
-
-    if (copy != NULL) {
-        // Print the copied string
-        printf("Copied string: %s\n", copy);
-
-        // Free the allocated memory
-        free(copy);
-    }
-
-    return 0;
-}
-
+/*#include <stdio.h>
+int	main()
+{
+	printf("%s", ft_strdup("test"));
+}*/
