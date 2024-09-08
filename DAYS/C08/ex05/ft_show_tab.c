e51972b75b80710f15df848715a2c9334d7631e7
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-09-08 23:39:19 +0100

    Update ft_show_tab.c

--- DAYS/C08/ex05/ft_show_tab.c
+++ DAYS/C08/ex05/ft_show_tab.c
@@ -1,34 +1,36 @@
 #include <unistd.h>
 #include "ft_stock_str.h"
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
 void	ft_putstr(char *str)
 {
-	while (*str)
-		write(1, str++, 1);
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write (1, &str[i], 1);
+		i++;
+	}
+}
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
 }
 
 void	ft_putnbr(int nb)
 {
-	unsigned int	n;
+	long int	n;
 
 	n = nb;
-	if (nb < 0)
+	if (n < 0)
 	{
 		ft_putchar('-');
 		n *= -1;
 	}
-	if (n < 10)
-		ft_putchar(n + '0');
-	else
-	{
+	if (nb >= 10)
 		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
-	}
+	ft_putchar((n % 10) + 48);
 }
 
 void	ft_show_tab(struct s_stock_str *par)
@@ -39,11 +41,11 @@ void	ft_show_tab(struct s_stock_str *par)
 	while (par[i].str)
 	{
 		ft_putstr(par[i].str);
-		ft_putchar('\n');
+		write (1, "\n", 1);
 		ft_putnbr(par[i].size);
-		ft_putchar('\n');
+		write (1, "\n", 1);
 		ft_putstr(par[i].copy);
-		ft_putchar('\n');
+		write (1, "\n", 1);
 		i++;
 	}
 }
