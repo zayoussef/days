1e7b7c3331d70cdea44c34e25a5cb39085e2d44e
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-09-08 23:28:44 +0100

    Update ft_fibonacci.c

--- DAYS/C05/ex04/ft_fibonacci.c
+++ DAYS/C05/ex04/ft_fibonacci.c
@@ -4,28 +4,19 @@
    Note: Overflow is not handled, so the result may be undefined in such cases.
 */
 
-int ft_fibonacci(int index)
+int	ft_fibonacci(int index)
 {
-    if (index < 0) // Check if the index is less than 0
-        return -1;
-    else if (index == 0) // Base case: Fibonacci sequence at index 0 is 0
-        return 0;
-    else if (index == 1 || index == 2) // Base case: Fibonacci sequence at index 1 and 2 is 1
-        return 1;
-    else
-        return ft_fibonacci(index - 1) + ft_fibonacci(index - 2); // Recursive call
-}
+	int	nb;
 
+	if (index < 0)
+		return (-1);
+	if (index < 2)
+		return (index);
+	nb = ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
+	return (nb);
+}
+/*#include <stdio.h>
 int main()
 {
-    int index = 6; // Example index
-
-    // Get the Fibonacci sequence element at the given index using ft_fibonacci function
-    int result = ft_fibonacci(index);
-
-    // Print the result
-    printf("The element at index %d in the Fibonacci sequence is: %d\n", index, result);
-
-    return 0;
-}
-
+	printf("is: %d\n", ft_fibonacci(15));
+}*/
