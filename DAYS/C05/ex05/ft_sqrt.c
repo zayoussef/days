30ab34e247fb25c7cfb3478992c291f0b37cd557
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-09-08 23:29:03 +0100

    Update ft_sqrt.c

--- DAYS/C05/ex05/ft_sqrt.c
+++ DAYS/C05/ex05/ft_sqrt.c
@@ -2,33 +2,27 @@
    if the square root is an irrational number.
 */
 
-int ft_sqrt(int nb)
+
+int	ft_sqrt(int nb)
 {
-    if (nb < 0) // Check if the number is negative
-        return 0;
+	int	i;
 
-    int sqrt = 1; // Variable to store the square root
-
-    while (sqrt * sqrt <= nb)
-    {
-        if (sqrt * sqrt == nb)
-            return sqrt;
-        sqrt++;
-    }
-
-    return 0; // Square root is not an integer
+	i = 1;
+	if (i > 0)
+	{
+		while (i * i <= nb)
+		{
+			if (i * i == nb)
+				return (i);
+			if (i > 46340)
+				return (0);
+			i++;
+		}
+	}
+	return (0);
 }
-
+/*#include <stdio.h>
 int main()
 {
-    int number = 25; // Example number
-
-    // Calculate the square root using the ft_sqrt function
-    int result = ft_sqrt(number);
-
-    // Print the result
-    printf("The square root of %d is: %d\n", number, result);
-
-    return 0;
-}
-
+	printf("is: %d\n", ft_sqrt(25));
+}*/
