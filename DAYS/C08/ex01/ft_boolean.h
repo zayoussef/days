4a72eede50a63f66dcb32f47554964367ffca10f
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-09-08 23:36:07 +0100

    Update ft_boolean.h

--- DAYS/C08/ex01/ft_boolean.h
+++ DAYS/C08/ex01/ft_boolean.h
@@ -2,15 +2,14 @@
 # define FT_BOOLEAN_H
 
 # include <unistd.h>
-# define EVEN(x) ((x) % 2 == 0)  // Enclosed in parentheses
 
+# define EVEN( nbr ) (nbr % 2 == 0) ? 1 : 0
 # define TRUE 1
 # define FALSE 0
 # define EVEN_MSG "I have an even number of arguments.\n"
-# define ODD_MSG "I have an odd number of arguments.\n"
+# define ODD_MSG "I have an add number of arguments.\n"
 # define SUCCESS 0
 
-typedef int t_bool;
+typedef int		t_bool;
 
 #endif
-
