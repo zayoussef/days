07e423fe46417066034cef90b034cfc0bd0b714d
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-09-08 23:44:44 +0100

    Update ft_show_tab.c and C09

--- DAYS/C08/ex05/ft_show_tab.c
+++ DAYS/C08/ex05/ft_show_tab.c
@@ -53,8 +53,9 @@ void	ft_show_tab(struct s_stock_str *par)
 
 -----------------------  C09  ----------------------
 
+ex00:
+	
 #!/bin/bash
-
 cc -Wall -Wextra -Werror -c ft_putchar.c ft_putstr.c ft_swap.c ft_strlen.c ft_strcmp.c
 ar -rcs libft.a ft_putchar.o ft_putstr.o ft_swap.o ft_strlen.o ft_strcmp.o
 
