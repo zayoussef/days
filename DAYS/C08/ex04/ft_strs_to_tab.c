cbc02a7346bbd1730c84990284ff9b3a194ebb14
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-09-08 23:38:42 +0100

    Update ft_strs_to_tab.c

--- DAYS/C08/ex04/ft_strs_to_tab.c
+++ DAYS/C08/ex04/ft_strs_to_tab.c
@@ -1,49 +1,55 @@
-#include <unistd.h>
+
+#include <stdlib.h>
 #include "ft_stock_str.h"
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	while (*str)
-		write(1, str++, 1);
-}
-
-void	ft_putnbr(int nb)
-{
-	unsigned int	n;
-
-	n = nb;
-	if (nb < 0)
-	{
-		ft_putchar('-');
-		n *= -1;
-	}
-	if (n < 10)
-		ft_putchar(n + '0');
-	else
-	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
-	}
-}
-
-void	ft_show_tab(struct s_stock_str *par)
+int	ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (par[i].str)
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *str)
+{
+	int		i;
+	int		j;
+	char	*ptr;
+
+	i = 0;
+	j = ft_strlen(str);
+	ptr = (char *) malloc(j + 1);
+	if (ptr == NULL)
+		return (NULL);
+	while (str[i])
 	{
-		ft_putstr(par[i].str);
-		ft_putchar('\n');
-		ft_putnbr(par[i].size);
-		ft_putchar('\n');
-		ft_putstr(par[i].copy);
-		ft_putchar('\n');
+		ptr[i] = str[i];
 		i++;
 	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
+{
+	t_stock_str		*j;
+	int				i;
+
+	j = malloc((ac + 1) * sizeof(t_stock_str));
+	if (j == NULL)
+		return (NULL);
+	i = 0;
+	while (i < ac)
+	{
+		j[i].size = ft_strlen(av[i]);
+		j[i].str = av[i];
+		j[i].copy = ft_strdup(av[i]);
+		i++;
+	}
+	j[i].str = 0;
+	j[i].size = 0;
+	j[i].copy = 0;
+	return (j);
 }
