fe0bc8e22d30cbafe33c332938c00d9d4606eaa7
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-09-08 23:25:42 +0100

    Update ft_rev_params.c

--- DAYS/C06/ex02/ft_rev_params.c
+++ DAYS/C06/ex02/ft_rev_params.c
@@ -1,27 +1,26 @@
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write (1, &c, 1);
 }
 
-int main(int argc, char *argv[])
+int	main(int ac, char **av)
 {
-    int i;
-    int j;
+	int	i;
+	int	j;
 
-    i = argc - 1;
-    while (i > 0)
-    {
-        j = 0;
-        while (argv[i][j] != '\0' && argc)
-        {
-            ft_putchar(argv[i][j]);
-            j++;
-        }
-        ft_putchar('\n');
-        i--;
-    }
-    return 0;
+	j = ac - 1;
+	while (j > 0)
+	{
+		i = 0;
+		while (av[j][i] != '\0')
+		{
+			ft_putchar(av[j][i]);
+			i++;
+		}
+		ft_putchar('\n');
+		j--;
+	}
+	return (0);
 }
-
