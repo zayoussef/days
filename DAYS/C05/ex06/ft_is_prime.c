84e4083790a548df22cd05cd52da43598de55cd7
zayoussef <136023225+zayoussef@users.noreply.github.com>
2024-09-08 23:29:19 +0100

    Update ft_is_prime.c

--- DAYS/C05/ex06/ft_is_prime.c
+++ DAYS/C05/ex06/ft_is_prime.c
@@ -3,36 +3,25 @@
    Note: The function considers 0 and 1 as non-prime numbers.
 */
 
-int ft_is_prime(int nb)
+int	ft_is_prime(int nb)
 {
-    if (nb <= 1) // Check if the number is less than or equal to 1
-        return 0;
+	int	i;
 
-    int divisor = 2; // Start with divisor 2
-
-    while (divisor * divisor <= nb)
-    {
-        if (nb % divisor == 0)
-            return 0; // Number is divisible, not prime
-        divisor++;
-    }
-
-    return 1; // Number is prime
+	i = 2;
+	if (nb >= 2)
+	{
+		while (i <= nb / i)
+		{
+			if (nb % i == 0)
+				return (0);
+			i++;
+		}
+		return (1);
+	}
+	return (0);
 }
-
+/*#include <stdio.h>
 int main()
 {
-    int number = 17; // Example number
-
-    // Check if the number is prime using ft_is_prime function
-    int result = ft_is_prime(number);
-
-    // Print the result
-    if (result == 1)
-        printf("%d is a prime number.\n", number);
-    else
-        printf("%d is not a prime number.\n", number);
-
-    return 0;
-}
-
+	printf("is: %d\n", ft_is_prime(7));
+}*/
